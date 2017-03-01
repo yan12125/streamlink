0c822ee4edda54b70df7898ee6086e9079907a91
Yen Chi Hsuan <yan12125@gmail.com>
2017-03-02 01:35:23 +0800

    plugins.douyutv: Switched to HLS

--- src/streamlink/plugins/douyutv.py
+++ src/streamlink/plugins/douyutv.py
@@ -1,17 +1,12 @@
-import hashlib
 import re
-import time
-import uuid
 
 from requests.adapters import HTTPAdapter
 
 from streamlink.plugin import Plugin
 from streamlink.plugin.api import http, validate, useragents
-from streamlink.stream import HTTPStream
+from streamlink.stream import HLSStream
 
 MAPI_URL = "https://m.douyu.com/html5/live?roomId={0}"
-LAPI_URL = "https://www.douyu.com/lapi/live/getPlay/{0}"
-LAPI_SECRET = "A12Svb&%1UUmf@hC"
 SHOW_STATUS_ONLINE = 1
 SHOW_STATUS_OFFLINE = 2
 STREAM_WEIGHTS = {
@@ -60,17 +55,8 @@ _room_schema = validate.Schema(
             "show_status": validate.all(
                 validate.text,
                 validate.transform(int)
-            )
-        })
-    },
-    validate.get("data")
-)
-
-_lapi_schema = validate.Schema(
-    {
-        "data": validate.any(None, {
-            "rtmp_url": validate.text,
-            "rtmp_live": validate.text
+            ),
+            "hls_url": validate.text
         })
     },
     validate.get("data")
@@ -115,54 +101,9 @@ class Douyutv(Plugin):
             self.logger.info("Stream currently unavailable.")
             return
 
-        ts = int(time.time() / 60)
-        did = uuid.uuid4().hex.upper()
-        sign = hashlib.md5(("{0}{1}{2}{3}".format(channel, did, LAPI_SECRET, ts)).encode("utf-8")).hexdigest()
-
-        data = {
-            "cdn": "ws",
-            "rate": "0",
-            "tt": ts,
-            "did": did,
-            "sign": sign
-        }
-
-        res = http.post(LAPI_URL.format(channel), data=data)
-        room = http.json(res, schema=_lapi_schema)
-
-        url = "{room[rtmp_url]}/{room[rtmp_live]}".format(room=room)
-        stream = HTTPStream(self.session, url)
-        yield "source", stream
-
-        data = {
-            "cdn": "ws",
-            "rate": "2",
-            "tt": ts,
-            "did": did,
-            "sign": sign
-        }
-
-        res = http.post(LAPI_URL.format(channel), data=data)
-        room = http.json(res, schema=_lapi_schema)
-
-        url = "{room[rtmp_url]}/{room[rtmp_live]}".format(room=room)
-        stream = HTTPStream(self.session, url)
-        yield "middle", stream
-
-        data = {
-            "cdn": "ws",
-            "rate": "1",
-            "tt": ts,
-            "did": did,
-            "sign": sign
-        }
-
-        res = http.post(LAPI_URL.format(channel), data=data)
-        room = http.json(res, schema=_lapi_schema)
-
-        url = "{room[rtmp_url]}/{room[rtmp_live]}".format(room=room)
-        stream = HTTPStream(self.session, url)
-        yield "low", stream
+        url = room["hls_url"]
+        stream = HLSStream(self.session, url)
+        yield "live", stream
 
 
 __plugin__ = Douyutv
